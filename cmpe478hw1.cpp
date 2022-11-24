df532d50f8cf0461240bf842599168f13fd6f211
acyishere <acyazici@outlook.com>
2022-11-25 01:42:18 +0300

    map works nicely

--- cmpe478hw1.cpp
+++ cmpe478hw1.cpp
@@ -10,11 +10,13 @@ vector<string> readFile(string fileName) {
     {
         content.push_back(str);
     }
+    return content;
 }
 
 int main() {
+
     // content holds the list of lines
-    vector<string> content = readFile("input.txt");
+    vector<string> content = readFile("fool.txt");
     // create a map named map
     map<string, vector<string>> map;
     // we will split the lines into words, 2nd value will be the key, 1st value will be the value in the map, if the key is not in the map, we will add it, if it is in the map, we will update it
@@ -32,16 +34,14 @@ int main() {
             }
         }
         words.push_back(word);
+        //cout << words[0] << " " << words[1] << endl;
         if (map.find(words[1]) == map.end()) {
-            map[words[1]] = { words[0] };
+            map[words[1]] =  {words[0]};
         }
         else {
             map[words[1]].push_back(words[0]);
         }
     }
-    
     // Now we have the map with 2nd value as key and 1st value as vector of values
-
-
     return 0;
 }
